059f92491244f9e7d271595193cf29c50d8ba497
Alexander Akulich <akulichalexander@gmail.com>
2021-12-15 22:49:55 +0300

    Protocol: Add InfClassPlayer network object

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -1309,6 +1309,15 @@ void CGameClient::OnNewSnapshot()
 					}
 				}
 			}
+			else if(Item.m_Type == NETOBJTYPE_INFCLASSPLAYER)
+			{
+				const CNetObj_InfClassPlayer *pPlayerData = (const CNetObj_InfClassPlayer *)pData;
+
+				if(Item.m_ID < MAX_CLIENTS)
+				{
+					ProcessInfClassPlayerInfo(Item.m_ID, pPlayerData);
+				}
+			}
 			else if(Item.m_Type == NETOBJTYPE_DDNETCHARACTER)
 			{
 				const CNetObj_DDNetCharacter *pCharacterData = (const CNetObj_DDNetCharacter *)pData;
@@ -2015,6 +2024,9 @@ void CGameClient::CClientData::Reset()
 	m_SkinInfo.m_ColorFeet = ColorRGBA(1, 1, 1);
 	m_SkinInfo.m_SkinMetrics.Reset();
 
+	m_InfClassPlayerFlags = 0;
+	m_InfClassPlayerClass = -1;
+
 	m_Solo = false;
 	m_Jetpack = false;
 	m_NoCollision = false;
@@ -2605,6 +2617,14 @@ vec2 CGameClient::GetSmoothPos(int ClientID)
 		}
 	}
 	return Pos;
+}
+
+void CGameClient::ProcessInfClassPlayerInfo(int ClientID, const CNetObj_InfClassPlayer *pPlayerData)
+{
+	CClientData *pClient = &m_aClients[ClientID];
+
+	pClient->m_InfClassPlayerFlags = pPlayerData->m_Flags;
+	pClient->m_InfClassPlayerClass = pPlayerData->m_Class;
 }
 
 void CGameClient::Echo(const char *pString)
