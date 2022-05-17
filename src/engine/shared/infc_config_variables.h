a2b6ef58a16bee2bb6f768496634db614ba4215b
Alexander Akulich <akulichalexander@gmail.com>
2022-05-18 00:53:25 +0300

    Introduce infc config options

---
+++ src/engine/shared/infc_config_variables.h
@@ -0,0 +1,4 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+
+MACRO_CONFIG_INT(InfcShowBoomerWeapon, infc_show_boomer_weapon, 0, 0, 1, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Toggle the Boomer hammer visibility")
